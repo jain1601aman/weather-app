e1e44e33c7fdb1ecbc530a8b421692aaabacd018
jain1601aman <54712796+jain1601aman@users.noreply.github.com>
2020-02-04 15:01:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//var htp = require('http');
+
 var f = require('fs');
 var express = require('express');
 var app = express();
@@ -26,21 +26,3 @@ app.get('/:city', function(req,res){
     });   
 });
 app.listen(7777);
-/*htp.createServer(function(req,res){
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=Indore,IN&appid=be38ae2e8f86831cbaf3426f353b8faf&units=metric'
-    var req = require('request');    
-    req(url , function(error,resp,body){
-        if(resp.statusCode === 200){
-            var data = JSON.parse(body); 
-            res.writeHead(200 , {'Content-Type' : 'text/html'});
-            var str = f.readFileSync(__dirname + '/we.htm' , 'utf8');
-            str = str.replace('{cname}',data['name']);
-            str = str.replace('{ctemp}',data.main.temp);
-            str = str.replace('{chmd}',data.main['humidity']);
-            str = str.replace('{cpress}',data.main['pressure']);
-            str = str.replace('{cwnd}',data.wind['speed']);
-            str = str.replace('{cwnddir}',data.wind['deg']);
-            res.end(str);
-        }
-    });    
-}).listen(7777);*/
